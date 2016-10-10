4f6e76aead845f0176d376ab78306b3907f7b552
Enrique José Ramón Balmaseda <ejrbalma@gmail.com>
2016-10-10 17:31:12 +0100

    ADD Libraries Histogram

---
+++ src/CalculateHistogram.java
@@ -0,0 +1,11 @@
+public class CalculateHistogram {
+
+    public static <T> Histogram<T> computeHistogram(T[] vector) {
+        Histogram<T> histo = new Histogram<>();
+
+        for (T key : vector) {
+            histo.increment(key);
+        }
+        return histo;
+    }
+}
